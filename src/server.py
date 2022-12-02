d26bf2074e86ec6ff418d7d3365196764ca78ee3
elenacalcan26 <elena.calcan26@gmail.com>
2022-12-03 00:09:51 +0200

    update country by id

--- src/server.py
+++ src/server.py
@@ -15,10 +15,8 @@ db_connection=mysql.connect(
 
 cursor = db_connection.cursor()
 
-print("Connected")
-
 @app.route('/api/countries', methods=["GET", "POST"])
-def bun():
+def countries_op():
     if request.method == "POST":
         smth = request.json
         if smth is None:
@@ -58,6 +56,32 @@ def bun():
             country_resp.append(obj)
 
         return Response(status=200, mimetype="json/application", response=json.dumps(country_resp))
+
+@app.route('/api/countries/<id>', methods=["PUT", "DELETE"])
+def country_processing(id):
+    if request.method == "PUT":
+        smth = request.json
+
+        if "nume" not in smth or "lat" not in smth or "lon" not in smth:
+            return Response(status=400)
+
+        sel_country_query = """SELECT * FROM Country WHERE id = %s"""
+        cursor.execute(sel_country_query, (int(id), ))
+        selected_country = cursor.fetchone()
+
+        if selected_country is None:
+            return Response(status=400)
+
+        update_country_query = """UPDATE Country SET country_name = %s, latitude = %s, longitude = %s WHERE id = %s"""
+        data = (smth["nume"], float(smth["lat"]), float(smth["lon"]), int(id))
+        try:
+            cursor.execute(update_country_query, data)
+        except:
+            return Response(status=400)
+        db_connection.commit()
+        return Response(status=200)
+
+    return 'GG'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
